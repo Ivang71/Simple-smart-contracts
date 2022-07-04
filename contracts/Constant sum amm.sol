279f1c2723ec39ca774fe45db9c12782ef0b7890
Artem Tolochkov <artemtolv@gmail.com>
2022-07-04 10:05:22 +0300

    Finish creating csamm contract

--- contracts/Constant sum amm.sol
+++ contracts/Constant sum amm.sol
@@ -31,7 +31,7 @@ contract CSAMM {
         totalSupply -= _amount;
     }
 
-    function update(uint _reserve0, uint _reserve1) private {
+    function _update(uint _reserve0, uint _reserve1) private {
         reserve0 = _reserve0;
         reserve1 = _reserve1;
     }
@@ -60,6 +60,40 @@ contract CSAMM {
         update(res0, res1);
         tokenOut.transfer(msg.sender, _amount);
     }
-    // addLiquidity
-    // removeLiquidity
+
+    function addLiquidity(uint amount0, uint amount1) external returns (uint shares) {
+        token0.transferFrom(msg.sender, address(this), amount0);
+        token1.transferFrom(msg.sender, address(this), amount1);
+
+        bal0 = token0.balanceOf(address(this));
+        bal1 = token1.balanceOf(address(this));
+        
+        d0 = bal0 - reserve0;
+        d1 = bal1 - reserve1;
+
+        if (totalSupply == 0) {
+            shares = bal0 + bal1;
+        } else {
+            shares = ((d0 + d1) * totalSupply) / (reserve0 + reserve1);
+        }
+        
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+        _update(bal0, bal1);
+    }
+    
+    function removeLiquidity(uint _shares) external returns (uint d0, uint d1) {
+        d0 = (reserve0 * _shares) / totalSupply;
+        d1 = (reserve0 * _shares) / totalSupply;
+
+        _burn(msg.sender, amount);
+        _update(reserve0 - d0, reserve1 - d1);
+
+        if (d0 > 0) {
+            token0.transfer(msg.sender, d0);
+        }
+        if (d1 > 0) {
+            token1.transfer(msg.sender, d1);
+        }
+    }
 }
